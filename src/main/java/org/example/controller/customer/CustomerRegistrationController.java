8ba2a3b5a2df9e623579a72596060643124309e4
VansiluKodikara <vansilu08@gmail.com>
2026-03-22 14:59:22 +0530

    Initial commit

---
+++ src/main/java/org/example/controller/customer/CustomerRegistrationController.java
@@ -0,0 +1,17 @@
+package org.example.controller.customer;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+
+public class CustomerRegistrationController {
+
+    public void btnCustBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustRegisterOnAction(ActionEvent actionEvent) {
+
+    }
+}
